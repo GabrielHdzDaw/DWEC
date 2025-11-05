8ecb332d677bca0fb121c4c1b90b50077f2ec107
= <=>
2025-11-06 00:28:12 +0100

    Auto push 2025-11-06 00:28:11

--- exercises/U1Project/src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const SERVER_URL: string = "http://localhost:3000";
-export const PROPERTIES_URL: string = `${SERVER_URL}/properties`;
-export const PROVINCES_URL: string = `${SERVER_URL}/provinces`;
--- exercises/U1Project/src/index.ts
+++
@@ -1,3 +0,0 @@
-const a: string = "lentejas";
-
-console.log(a);
